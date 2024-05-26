a46bb88526057d78c121e945313bd82c0378f701
IVELINCHO2024 <157578504+IVELINCHO2024@users.noreply.github.com>
2024-05-26 18:38:54 +0300

    background color changing buttons

---
+++ colour_changing.js
@@ -0,0 +1,22 @@
+function Red() {
+    document.body.style.backgroundColor = "red";
+    document.body.style.color="white";
+    document.getElementById("navigation").style.backgroundColor="darkred";
+  }
+function Blue(){
+    document.body.style.backgroundColor="blue";   
+    document.body.style.color="white"; 
+    document.getElementById("navigation").style.backgroundColor="darkblue";
+}
+function Dark(){
+    document.body.style.backgroundColor="rgb(58, 58, 58)";
+    document.body.style.color="white";
+    document.getElementById("navigation").style.backgroundColor="#252121";
+   
+}
+function Default(){
+    document.body.style.backgroundColor="white";
+    document.body.style.color="black";
+    document.getElementById("navigation").style.backgroundColor=" #383947";
+   
+}
